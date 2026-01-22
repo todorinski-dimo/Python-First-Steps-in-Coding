b3e66d56b3b529e47cda4bfca41b71dbf06eb6d7
Dimo Todorinski <todorinski.dimo@gmail.com>
2026-01-22 08:26:00 +0200

    Create 07_easter_gifts_100points.py
    #Fixed check for invalid input index. Check for negative values was missing.

---
+++ Fundamentals/Lists/07_easter_gifts_100points.py
@@ -0,0 +1,24 @@
+gifts_list = input().split()
+
+while True:
+    cmd = input()
+    if cmd == "No Money":
+        break
+    cmd = cmd.split()
+    if cmd[0] == "OutOfStock":
+        for gift_pos in range(len(gifts_list)):
+            if gifts_list[gift_pos] == cmd[1]:
+                gifts_list[gift_pos] = "None"
+    elif cmd[0] == "Required":
+      
+#Fixed check for invalid input index. Check for negative values was missing.
+        if 0 <= int(cmd[2]) < len(gifts_list):
+            gifts_list[int(cmd[2])] = cmd[1]
+    elif cmd[0] == "JustInCase":
+        gifts_list[-1] = cmd[1]
+
+for gift_pos in range(len(gifts_list)):
+    if gifts_list[gift_pos] != "None":
+        print(f"{gifts_list[gift_pos]}", end="")
+        if gift_pos < len(gifts_list) - 1:
+            print(" ", end="")
