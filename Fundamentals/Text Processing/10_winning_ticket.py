da3c6525983e56fd738ca667545d4b597b7f94ee
Dimo Todorinski <todorinski.dimo@gmail.com>
2026-02-22 19:42:17 +0200

    changes to homeworks

--- Fundamentals/Text Processing/10_winning_ticket.py
+++ Fundamentals/Text Processing/10_winning_ticket.py
@@ -3,23 +3,23 @@ tickets = input().split(", ")
 for index in range(len(tickets)):
     tickets[index] = tickets[index].strip()
 
-
+match = False
 wining_chars = ['@','#','$','^']
 for item in tickets:
+    match = False
     if len(item) != 20:
         print("invalid ticket")
         continue
     for ch in wining_chars:
-            for multy in range(10, 5, -1):
-                find_chars = ch * multy
-                left_ticket = item[0:10]
-                right_ticket = item[10:20]
-                if left_ticket.find(find_chars) > -1 and right_ticket.find(find_chars) > -1 and len(find_chars) == 10:
-                    message = True
-                    print(f'ticket "{item}" - {len(find_chars)}{ch} Jackpot!')
-                elif left_ticket.find(find_chars) > -1 and right_ticket.find(find_chars) > -1 and 6 <= len(find_chars) <= 9:
-                    message = True
-                    print(f'ticket "{item}" - {len(find_chars)}{ch}')
-                else:
-                    message = True
-                    print(f'ticket "{item}" - no match')
+        for multy in range(10, 5, -1):
+            find_chars = ch * multy
+            left_ticket = item[0:10]
+            right_ticket = item[10:20]
+            if left_ticket.find(find_chars) > -1 and right_ticket.find(find_chars) > -1 and len(find_chars) == 10 and not match:
+                match = True
+                print(f'ticket "{item}" - {len(find_chars)}{ch} Jackpot!')
+            elif left_ticket.find(find_chars) > -1 and right_ticket.find(find_chars) > -1 and 6 <= len(find_chars) <= 9 and not match:
+                match = True
+                print(f'ticket "{item}" - {len(find_chars)}{ch}')
+    if not match:
+        print(f'ticket "{item}" - no match')
